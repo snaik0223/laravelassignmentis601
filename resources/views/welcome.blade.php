c4b3fde83bfae749ffd476147c51f9e8b1d98305
Shamix <nsammi11@gmail.com>
2018-10-16 22:37:40 -0400

    Task: Edited Welcome content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Welcome to the Shamix homepage
+                   Welcome to Shamix homepage
                 </div>
 
                 <div class="links">
