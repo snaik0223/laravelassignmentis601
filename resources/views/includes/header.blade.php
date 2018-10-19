639fe31bd608fd16f5a5f4f2fd7b6a2e36742520
Shamix <nsammi11@gmail.com>
2018-10-18 22:32:52 -0400

    Task: Change from ABOUT to HOME

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -10,7 +10,7 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item ">
-                        <a class="nav-link" href="/">ABOUT <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/">HOME <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/About">ABOUT</a>
